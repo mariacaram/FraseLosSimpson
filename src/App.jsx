81cd90fad65266a48da1ef4cd969d74c2a06e183
Maria <maria.caram@gmail.com>
2022-05-13 20:01:42 -0300

    Se agrega fetch para consultar API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import './App.css';
-import logo from './logofamilia.png.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-import Frases from './components/Frases';
+import "./App.css";
+import logo from "./logofamilia.png.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import Frases from "./components/Frases";
+import { useEffect, useState } from "react";
 function App() {
+  const [personaje, setPersonaje] = useState({});
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+  const consultarAPI = async() => {
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const dato = await respuesta.json();
+    console.log(respuesta);
+    console.log(dato)
+    setPersonaje(dato[0])
+  };
+
+
   return (
-    <section className='container my-5 d-flex flex-column align-items-center'>
-<img src={logo} alt="logo de los simpson" />
-<Button variant= 'warning' className='my-5 w-50' >Obtener frases</Button>
-  <Frases></Frases>
-  </section>
+    <section className="container my-5 d-flex flex-column align-items-center">
+      <img src={logo} alt="logo de los simpson" />
+      <Button variant="warning" className="my-5 w-50">
+        Obtener frases
+      </Button>
+      <Frases></Frases>
+    </section>
   );
 }
 
 export default App;
+
